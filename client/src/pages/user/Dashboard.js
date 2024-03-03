5c65cde0a18c9a4fcbdda0e20d2d430b1edc186f
masoom <masoomshaikh4874@gmail.com>
2024-03-03 23:07:23 +0530

    categoty api completed

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -15,7 +15,8 @@ const Dashboard = () => {
           <div className="col-md-9">
             <div className="card w-75 p-3">
               <h3>{auth?.user?.name}</h3>
-              <h3>{auth?.user?.email}</h3> <h3>{auth?.user?.address}</h3>
+              <h3>{auth?.user?.email}</h3>
+              <h3>{auth?.user?.address}</h3>
             </div>
           </div>
         </div>
